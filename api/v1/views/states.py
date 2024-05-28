e80237629ea985dac4918fe724ec5579c9fb9ee1
JuggernautTej <tiwatej@gmail.com>
2024-05-28 16:57:40 +0100

    another edit to states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Handles all default RESTful actions"""
 
-from flask import Flask, abort, jsonify, request
+from flask import abort, jsonify, request
 from api.v1.views import app_views
 from models import storage
 from models.state import State
