54f83610adb45f7d9688f7e76ebe3a8810f78e67
JuggernautTej <tiwatej@gmail.com>
2024-05-28 17:05:08 +0100

    edited method definitions in states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,7 +8,7 @@ from models.state import State
 
 
 @app_views.route("/states", methods=["GET"], strict_slashes=False)
-def all_state_objs:
+def all_state_objs():
     """Retrieves the list of all state objects"""
     all_states = storage.all(State)
     list_state = [state.to_dict() for state in all_states.values()]
@@ -17,7 +17,7 @@ def all_state_objs:
 
 @app_views.route("/api/v1/states/<state_id>",
                  methods=["GET"], strict_slashes=False)
-def state_objs:
+def state_objs(state_id):
     """This retrieves a single state object"""
     state = storage.get(State, state_id)
     if not state:
@@ -27,7 +27,7 @@ def state_objs:
 
 @app_views.route("/api/v1/states/<state_id>",
                  methods=["DELETE"], strict_slashes=False)
-def del_state:
+def del_state(state_id):
     """Delects state objext"""
     state = storage.get(State, state_id)
     if not state:
@@ -40,7 +40,7 @@ def del_state:
 
 
 @app_views.route("/api/v1/states", methods=["POST"], strict_slashes=False)
-def creates_state:
+def creates_state():
     """creates a state object"""
     try:
         state = request.get_json()
@@ -56,7 +56,7 @@ def creates_state:
 
 @app_views.route("/api/v1/states/<state_id>",
                  methods=["PUT"], strict_slashes=False)
-def update_state:
+def update_state(state_id):
     """This updates the state object"""
     state = storage.get(State, state_id)
     if not state:
